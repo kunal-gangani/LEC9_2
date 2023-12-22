9b0484c90dd283606a0fbb8543e7b846feb5527e
Kunal Gangani <thekunalgangani@gmail.com>
2023-12-22 15:30:32 +0530

    Added

---
+++ LEC9_2_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    int i,length;
+    char username[100],password;
+    int letter,digit,special;
+    printf("Enter your Password :");
+    gets(username);
+    if(length<7){
+    	printf("Your Password is Strong");
+    	if(special,letter,digit){
+	 	printf("Your Password is Weak");
+	}else if (special,letter){
+		printf("Your password is Weak");
+	}
+}
+}
+
